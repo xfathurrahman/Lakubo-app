26fd8b2c8071dd76f87cb554113657cdbea79043
xfathurrahman <test@localhost>
2023-03-23 22:33:05 +0700

    modified:   .env.example 	modified:   app/Http/Kernel.php 	deleted:    app/Http/Middleware/SecureHeaders.php 	modified:   app/Providers/AppServiceProvider.php 	modified:   docker-compose.yml 	modified:   docker/nginx/nginx.conf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,9 +31,15 @@ class AppServiceProvider extends ServiceProvider
     {
         View::composer('home.components.navbar', NavbarComposer::class);
         // Fix https
-        if ((isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1)) || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
-            $this->app['request']->server->set('HTTPS', true);
+        // if ((isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1)) || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+        //     $this->app['request']->server->set('HTTPS', true);
+        // }
+
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS','on');
+            URL::forceSchema('https');
         }
+
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         Schema::defaultStringLength(191);
